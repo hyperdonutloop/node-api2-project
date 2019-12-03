f5f74906704b4f4cd429ab18d65f6abd75744ac6
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-03 14:33:05 -0800

    built out get by id GET request. tested with insomnia and it works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -6,7 +6,7 @@ const Posts = require('../data/db.js');
 
 router.use(express.json());
 
-// GET REQUEST 
+// GET Request api/posts
 // - 500 for error
 // - error message 
 router.get('/', (req, res) => {
@@ -15,11 +15,27 @@ router.get('/', (req, res) => {
     res.status(200).json(posts)
   })
   .catch(error => {
-    res.status(500).json({ errorMessage: 'The posts information could not be retrieved'})
+    res.status(500).json({ errorMessage: 'The posts information could not be retrieved' })
   })
 })
 
+// GET Request for post object with the specified id
+// if post with id is not found return 404 with error message
+// if there is error retrieving post return 500 and error message
 
+router.get('/:id', (req, res) => {
+  Posts.findById(req.params.id)
+  .then(posts => {
+    if (posts) {
+      res.status(200).json(posts)
+    } else {
+      res.status(404).json({ errorMessage: 'The post with the specified ID does not exist' })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ errorMessage: 'The post information could not be retrieved' })
+  })
+})
 
 
 module.exports = router;
